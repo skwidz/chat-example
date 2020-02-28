25eef1815035e2a8c5106603c004f059dc80ae65
skwidz <christian.e@getjobber.com>
2020-02-28 13:32:04 -0700

    add some logging for example

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3333;
 
 app.get('/', function(req, res){
+  console.log("incoming request")
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  console.log("user connected")
   socket.on('chat message', function(msg){
+    console.log("new message: " + msg)
     io.emit('chat message', msg);
   });
 });
